d3738f7203de9ccd19e79c91c285199b14d4d46b
iliasbtk <ilias.betkaoui@gmail.com>
2021-05-31 07:35:37 +0100

    #003 Add toggle button to handle closing and opening the navigation drawer

--- app/src/main/java/com/example/speedbumpdetection/MainActivity.java
+++ app/src/main/java/com/example/speedbumpdetection/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.speedbumpdetection;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    private DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.layout_drawer);
+        //Drawer Button
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+    }
+
+    //Handle the phone back button when drawer is open
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+
+        }
+
     }
 }
