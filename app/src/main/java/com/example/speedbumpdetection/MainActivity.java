5bc21504bce9534feb0edf8dfc910293b4058ce4
iliasbtk <ilias.betkaoui@gmail.com>
2021-06-09 10:03:37 +0100

    #005 Handle Menu item clicks

--- app/src/main/java/com/example/speedbumpdetection/MainActivity.java
+++ app/src/main/java/com/example/speedbumpdetection/MainActivity.java
@@ -1,18 +1,25 @@
 package com.example.speedbumpdetection;
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Toolbar toolbar;
     private DrawerLayout drawer;
+    NavigationView navigationView;
+    Intent i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.layout_drawer);
+
+        navigationView = findViewById(R.id.view_nav);
+        navigationView.setNavigationItemSelectedListener(this);
+
         //Drawer Button
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -30,6 +41,23 @@ public class MainActivity extends AppCompatActivity {
         toggle.syncState();
 
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_map:
+                i = new Intent(this, MapActivity.class);
+                startActivity(i);
+                break;
+            case R.id.nav_settings:
+                i = new Intent(this, SettingsActivity.class);
+                startActivity(i);
+                break;
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     //Handle the phone back button when drawer is open
@@ -43,4 +71,6 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
 }
