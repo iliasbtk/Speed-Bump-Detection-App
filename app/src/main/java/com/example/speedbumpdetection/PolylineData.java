ef7d841c38ef8774402bc31652b523e02c138f8d
iliasbtk <ilias.betkaoui@gmail.com>
2021-06-17 15:26:13 +0100

    #017 Display routes duration and distance

---
+++ app/src/main/java/com/example/speedbumpdetection/PolylineData.java
@@ -0,0 +1,48 @@
+package com.example.speedbumpdetection;
+
+import com.google.android.gms.maps.model.Polyline;
+import com.google.maps.model.DirectionsLeg;
+
+public class PolylineData {
+    private Polyline polyline;
+    private DirectionsLeg directionsLeg;
+    private int speedBumpNumber;
+
+    public Polyline getPolyline() {
+        return polyline;
+    }
+
+    public int getSpeedBumpNumber() {
+        return speedBumpNumber;
+    }
+
+    public void setSpeedBumpNumber(int speedBumpNumber) {
+        this.speedBumpNumber = speedBumpNumber;
+    }
+
+    public DirectionsLeg getDirectionsLeg() {
+        return directionsLeg;
+    }
+
+    public void setPolyline(Polyline polyline) {
+        this.polyline = polyline;
+    }
+
+    public void setDirectionsLeg(DirectionsLeg directionsLeg) {
+        this.directionsLeg = directionsLeg;
+    }
+
+    public PolylineData(Polyline polyline, DirectionsLeg directionsLeg, int speedBumpNumber){
+        this.polyline = polyline;
+        this.directionsLeg = directionsLeg;
+        this.speedBumpNumber = speedBumpNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "PolylineData{" +
+                "polyline=" + polyline +
+                ", directionsLeg=" + directionsLeg +
+                '}';
+    }
+}
